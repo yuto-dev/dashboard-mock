97769b21e5e662f2bb57a02d4e4903e6377b4afb
Abiyyu Bagindo <directx178@gmail.com>
2024-07-24 02:09:37 +0700

    Updated login and powerpoint

--- src/features/user/Login.js
+++ src/features/user/Login.js
@@ -38,9 +38,9 @@ function Login(){
     return(
         <div className="min-h-screen bg-base-200 flex items-center">
             <div className="card mx-auto w-full max-w-5xl  shadow-xl">
-                <div className="grid  md:grid-cols-2 grid-cols-1  bg-base-100 rounded-xl">
+                <div className="grid  md:grid-cols-1 grid-cols-1  bg-base-100 rounded-xl">
                 <div className=''>
-                        <LandingIntro />
+                {/* <img src="./Logodagri.png" alt="PKB" className="w-full h-full object-cover rounded-lg" /> */}
                 </div>
                 <div className='py-24 px-10'>
                     <h2 className='text-2xl font-semibold mb-2 text-center'>Login</h2>
